6a108b9e20152c933d7f8eb6ae845e24678293f6
AVINASH MAURYA <146184986+LAZY-CODER01@users.noreply.github.com>
2025-07-12 10:28:23 +0530

    cors and connection established

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+const corsOptions = {
+  origin: ['*'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+app.use('/',(req,res)=>{
+  res.send('Welcome to the API');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
