565f60a5b383a9fd90ba1e0892e9f21159feca18
ayushrai6249 <ayushrai6249@gmail.com>
2025-07-12 12:54:48 +0530

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Home from './pages/Home';
+import Cloths from './pages/Cloths'
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
 import UserDashboard from './Components/UserDashboard';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cloths" element={<Cloths />} />
+        <Route path='/login' element={<Login />} />
         <Route path="/dashboard" element={<UserDashboard />} />
       </Routes>
     </div>
