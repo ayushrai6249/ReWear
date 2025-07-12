76cdad2a1bae06ff39d4177350137f612fe077e7
ayushrai6249 <ayushrai6249@gmail.com>
2025-07-12 11:39:11 +0530

    Add navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,14 @@
+
+import profile_pic from './profile_pic.png'
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.png'
+
+
+export const assets = {
+    profile_pic,
+    menu_icon,
+    cross_icon,
+    dropdown_icon,
+
+}
